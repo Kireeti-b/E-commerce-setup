d50e32ea57a597ca3078ba40736d07cc55f8ca89
Kireeti-b <Kireetibhyrovabhotla@gmail.com>
2023-10-18 10:03:55 -0400

    Basic settings to run the application and added home screen

--- src/main/java/com/magtech/projects/ECommerce/ECommerceApplication.java
+++ src/main/java/com/magtech/projects/ECommerce/ECommerceApplication.java
@@ -2,12 +2,24 @@ package com.magtech.projects.ECommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication @RestController
 public class ECommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApplication.class, args);
 	}
 
+	@GetMapping
+	public static String home(){
+		return """
+              <html>
+                  <body>
+                      <h1>Hello, Welcome to the E-commerce application</h1>
+                  </body>
+              </html>
+              """;
+	}
 }
