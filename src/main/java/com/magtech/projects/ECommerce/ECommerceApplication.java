63fcd3df37219ac156d58b6ea1569da5aa7f942b
Kireeti-b <Kireetibhyrovabhotla@gmail.com>
2023-10-18 08:46:31 -0400

    Setting up

---
+++ src/main/java/com/magtech/projects/ECommerce/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.magtech.projects.ECommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
